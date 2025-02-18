f7fff63209d491004039df860d70dc09dc47176d
Roshan Ram SA <roshanramsa@gmail.com>
2025-02-18 23:06:18 +0530

    Message

--- mainv5.js
+++ mainv5.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = 1800;
+canvas.width = 1900;
 canvas.height = 900;
 
 c.fillRect(0,0,canvas.width, canvas.height);
